6d7e34ab99d0ebeaa516476ae4050f18f6ce7a28
Sebastian Gonzalez <sebastian.gonzalez@glovoapp.com>
2024-03-02 09:17:38 +0100

    feat: add auth button in header

--- apps/my-finance/src/app/layout.tsx
+++ apps/my-finance/src/app/layout.tsx
@@ -1,20 +1,56 @@
+'use client'
+
 import clsx from 'clsx'
 import { Sarabun } from 'next/font/google'
 import { ToastContainer } from 'react-toastify'
 import 'react-toastify/dist/ReactToastify.min.css'
 
 import { BaseHeader, BaseLayout } from '@web-apps/ui'
+import { ArrowLeftToBracketIcon, ArrowRightToBracketIcon } from '@web-apps/ui'
+
+import { useAuth } from '@/hooks/auth'
+
+import { Authenticated, Unauthenticated } from '@/components/Authentication'
 
 import './globals.css'
 
 const font = Sarabun({ subsets: ['latin'], weight: ['200', '300', '400', '500', '600', '700', '800'] })
+
+const AuthButton = () => {
+  const { login, logout } = useAuth()
+
+  return (
+    <>
+      <Authenticated>
+        <button className="flex items-center gap-x-1 text-sm text-base-content-2" onClick={logout}>
+          <span>Sign out</span>
+          <ArrowRightToBracketIcon className="relative top-[1px] w-[20px] h-[20px]" />
+        </button>
+      </Authenticated>
+      <Unauthenticated>
+        <button className="flex items-center gap-x-1 text-sm text-base-content-2" onClick={() => login('sebastiangon11@gmail.com', '123456')}>
+          <span>Sign in</span>
+          <ArrowLeftToBracketIcon className="relative top-[1px] w-[20px] h-[20px]" />
+        </button>
+      </Unauthenticated>
+    </>
+  )
+}
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="en">
       <body className={`${font.className}`}>
         <BaseLayout
-          header={<BaseHeader className={clsx('py-2', 'justify-center', 'border-b border-white/10 border-x-slate-300')}>My Finances</BaseHeader>}
+          header={
+            <BaseHeader className={clsx('py-2 mx-4', 'justify-center items-center', 'border-b border-white/10 border-x-slate-300')}>
+              <div className="min-w-max mr-auto"></div>
+              <p className="text-lg">My Finances</p>
+              <div className="min-w-max ml-auto">
+                <AuthButton />
+              </div>
+            </BaseHeader>
+          }
           leftSidebar={<div className="h-full flex flex-col justify-center items-center"></div>}
           main={<main>{children}</main>}
           rightSidebar={<div className="h-full flex flex-col justify-center items-center"></div>}
---
+++ packages/ui/src/icons/arrows/index.tsx
@@ -0,0 +1,18 @@
+import clsx from 'clsx'
+
+type arrowIconProps = {
+  className?: string
+  hoverable?: boolean
+}
+
+export const ArrowLeftToBracketIcon = ({ className, hoverable = false }: arrowIconProps) => (
+  <svg className={clsx('w-[24px] h-[24px]', 'stroke-1', { 'hover:stroke-[1.5]': hoverable }, className)} aria-hidden fill="none" viewBox="0 0 24 24">
+    <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" d="M16 12H4m12 0-4 4m4-4-4-4m3-4h2a3 3 0 0 1 3 3v10a3 3 0 0 1-3 3h-2" />
+  </svg>
+)
+
+export const ArrowRightToBracketIcon = ({ className, hoverable = false }: arrowIconProps) => (
+  <svg className={clsx('w-[24px] h-[24px]', 'stroke-1', { 'hover:stroke-[1.5]': hoverable }, className)} aria-hidden fill="none" viewBox="0 0 24 24">
+    <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" d="M20 12H8m12 0-4 4m4-4-4-4M9 4H7a3 3 0 0 0-3 3v10a3 3 0 0 0 3 3h2" />
+  </svg>
+)
