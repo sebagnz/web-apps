48edd19b7277384eb43db65356614eb7077ac1b1
Sebastián González <sebastian.gonzalez@glovoapp.com>
2024-03-15 18:21:44 +0100

    feat: add snapshots routes

--- apps/my-finance/src/app/routes.tsx
+++ apps/my-finance/src/app/routes.tsx
@@ -7,6 +7,10 @@ export const Routes = {
       index: '/app/accounts',
       new: '/app/accounts/new',
       id: (id: string) => `/app/accounts/${id}`,
+      snapshots: {
+        new: (id: string) => `/app/accounts/${id}/snapshots/new`,
+        id: (id: string, snapshotId: string) => `/app/accounts/${id}/snapshots/${snapshotId}`,
+      },
     },
     expenses: {
       index: '/app/expenses',
