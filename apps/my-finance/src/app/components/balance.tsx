dc4e8d1ff4712780d1c60f577cfc6c225fd8645a
Sebastián González <sebastian.gonzalez@glovoapp.com>
2024-05-02 11:59:19 +0200

    feat: add hide balance animation

--- apps/my-finance/src/app/components/balance.tsx
+++ apps/my-finance/src/app/components/balance.tsx
@@ -1,4 +1,5 @@
-import { ComponentPropsWithoutRef } from 'react'
+import { CSSProperties, ComponentPropsWithoutRef, useRef } from 'react'
+import { SwitchTransition, Transition, TransitionStatus } from 'react-transition-group'
 
 import { usePreferences } from '@/hooks/preferences'
 
@@ -10,7 +11,25 @@ type BalanceProps = { currency?: keyof CurrencyType; children: number | string }
 
 type Props = Omit<ComponentPropsWithoutRef<'p'>, keyof BalanceProps> & BalanceProps
 
+const duration = 100
+
+const initialStyle = {
+  transition: `all ${duration}ms ease`,
+  opacity: 1,
+  scale: 1,
+}
+
+const transitionStyles: Record<TransitionStatus, CSSProperties> = {
+  entering: { scale: 1, opacity: 1 },
+  entered: { scale: 1, opacity: 1 },
+  exiting: { scale: 0.7, opacity: 0.7 },
+  exited: { scale: 0.7, opacity: 0.7 },
+  unmounted: { scale: 0.7, opacity: 0.7 },
+}
+
 export const Balance = ({ currency = 'EUR', children, ...rest }: Props) => {
+  const nodeRef = useRef(null)
+
   const { preferences } = usePreferences()
 
   if (!preferences) return null
@@ -18,8 +37,21 @@ export const Balance = ({ currency = 'EUR', children, ...rest }: Props) => {
   const textContent = preferences.hideBalances ? '••••••' : children.toLocaleString()
 
   return (
-    <p {...rest}>
-      {Currency[currency]} {textContent}
-    </p>
+    <SwitchTransition>
+      <Transition nodeRef={nodeRef} key={textContent} timeout={duration} unmountOnExit>
+        {(state) => (
+          <p
+            {...rest}
+            ref={nodeRef}
+            style={{
+              ...initialStyle,
+              ...transitionStyles[state],
+            }}
+          >
+            {Currency[currency]} {textContent}
+          </p>
+        )}
+      </Transition>
+    </SwitchTransition>
   )
 }
