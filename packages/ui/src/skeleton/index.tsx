d4f05f3a40155eb6bb2c4d9a5b191fde911ef088
Seba González <sebagnz93@gmail.com>
2024-06-07 23:58:33 +0200

    feat: add contrast to skeleton

--- packages/ui/src/skeleton/index.tsx
+++ packages/ui/src/skeleton/index.tsx
@@ -4,7 +4,7 @@ import { twMerge } from 'tailwind-merge'
 export const Skeleton = ({ className, ...props }: ComponentPropsWithoutRef<'div'>) => {
   return (
     <div role="status" className="animate-pulse" {...props}>
-      <div className={twMerge('min-h-2 bg-muted/50 rounded-lg', className)} />
+      <div className={twMerge('min-h-2 bg-muted rounded-lg', className)} />
       <span className="sr-only">Loading...</span>
     </div>
   )
