828d0e7790e3ab685c2cf71fc8976bfa12848af2
Seba González <sebagnz93@gmail.com>
2024-06-09 21:55:18 +0200

    fix: preferences default

--- apps/fingarden/src/app/domain/preferences/preferences.ts
+++ apps/fingarden/src/app/domain/preferences/preferences.ts
@@ -2,9 +2,11 @@ import { z } from 'zod'
 
 import { CurrencySchema } from '../currencies/currency'
 
-export const PreferencesSchema = z.object({
-  hideBalances: z.boolean().default(false),
-  mainCurrency: CurrencySchema.shape.code.default('USD'),
-})
+export const PreferencesSchema = z
+  .object({
+    hideBalances: z.boolean().default(false),
+    mainCurrency: CurrencySchema.shape.code.default('USD'),
+  })
+  .default({})
 
 export type Preferences = z.infer<typeof PreferencesSchema>
