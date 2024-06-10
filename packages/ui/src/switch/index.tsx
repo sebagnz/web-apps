a15d5591d1435133241ffca4be5b73a83cebb193
Seba González <sebagnz93@gmail.com>
2024-06-10 11:32:15 +0200

    feat: switch refactor

--- packages/ui/src/switch/index.tsx
+++ packages/ui/src/switch/index.tsx
@@ -12,14 +12,14 @@ export const Switch = ({ children, className, on, onChange, ...rest }: Props) =>
   const handleChange = (e: ChangeEvent<HTMLInputElement>) => onChange(e.target.checked)
 
   return (
-    <label>
+    <label className="flex">
       <input type="checkbox" onChange={handleChange} checked={on} className="peer sr-only" {...rest} />
       <span
         className={twMerge(
           'relative inline-block cursor-pointer outline-0 select-none rounded-full aspect-[2/1] p-[2px]',
           'transition-all duration-500 ease-[cubic-bezier(.25,.1,.25,1)]',
           'border border-base',
-          'bg-muted peer-checked:bg-accent',
+          'bg-accent-muted peer-checked:bg-accent',
           'after:block after:box-content after:relative after:content-[""] after:left-0 after:w-1/2 after:aspect-square after:rounded-full',
           'after:shadow-sm after:shadow-base',
           'after:bg-base',
